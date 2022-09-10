3a73988593d678d6a1fe2c6fca521a902cee8ded
taqin <abdulmuttaqin456@gmail.com>
2022-09-10 22:24:46 +0700

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,19 @@
-#!/usr/bin/env node
-const { argv } = require('process');
-const merger  = require('../lib/index.js');
+'use strict';
+
+var _require = require('process');
+
+var argv = _require.argv;
+
+var merger = require('../lib/index.js');
 
 // send help command
 
 
 if (argv.length < 3) {
     console.log('Usage: json-merger <folder> <output>');
-    return;
 }
 
 // get folder and output from command line
-const folder = argv[2];
-const output = argv[3];
+var folder = argv[2];
+var output = argv[3];
 merger(folder, output);
